8987365f000ab68cc30de315da7509ac306146ba
Kyungju.Kim <46181548+Kyungju-Kim@users.noreply.github.com>
2025-02-27 16:43:17 +0900

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "IMFINE-TerraXPacMan_ARGame-0.1.1";
+const cacheName = "IMFINE-TerraXPacMan_ARGame-0.1.7";
 const contentToCache = [
     "Build/PacMan.loader.js",
     "Build/PacMan.framework.js.unityweb",
